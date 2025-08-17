48e063c79c5e46b6ff0c42a02db9bbae0b61bd93
Yaksh Garg <gargyaksh8@gmail.com>
2025-08-17 20:24:15 +0530

    layout and boiler plate dome

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import './App.css'
+import {BrowserRouter, Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
+import Home from './Home'
+import About from './About'
 
 function App() {
 
   return (
     <>
-    <p className='text-5xl text-extrabold'>Quick Buy</p>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
+  )
+}
+
+function Layout() {
+  return (
+    <div className='h-screen '>
+      <div className='flex gap-5  justify-center h-[5vh] items-center'>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+      </div>
+      <div className='h-[90vh] flex flex-col justify-center items-center'>
+        <Outlet />
+      </div>
+      <div className='flex gap-5  justify-center h-[5vh] items-center'>
+        Footer
+      </div>
+    </div>
   )
 }
 
